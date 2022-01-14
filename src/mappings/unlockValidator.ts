2fae171bfd01a22a04f346dc2c106d6444a4b795
Satoshi Naoki <SenWebDev1009@gmail.com>
2022-01-14 11:09:57 +1000

    add UnlockValidator tracking

---
+++ src/mappings/unlockValidator.ts
@@ -0,0 +1,51 @@
+import {
+  InValidate,
+  Validate
+} from "../../generated/UnlockValidator/UnlockValidator";
+import {
+  InValidateEvent,
+  ValidateEvent
+} from "../../generated/schema";
+import {
+  createOrUpdateGlobal,
+  getOrCreateAccount,
+} from "./common";
+
+export function handleInValidateEvent(
+  _event: InValidate
+): void {
+  let account = getOrCreateAccount(
+    _event.params.account.toHexString(),
+    _event.block.timestamp
+  );
+
+  createOrUpdateGlobal(
+    'INVALIDATED_' + account.id,
+    'true'
+  );
+
+  let eventId = _event.transaction.hash.toHexString();
+  let event = new InValidateEvent(eventId);
+  event.account = account.id;
+  event.timestamp = _event.block.timestamp.toI32();
+  event.save();
+}
+export function handleValidateEvent(
+  _event: Validate
+): void {
+  let account = getOrCreateAccount(
+    _event.params.account.toHexString(),
+    _event.block.timestamp
+  );
+
+  createOrUpdateGlobal(
+    'INVALIDATED_' + account.id,
+    'false'
+  );
+
+  let eventId = _event.transaction.hash.toHexString();
+  let event = new ValidateEvent(eventId);
+  event.account = account.id;
+  event.timestamp = _event.block.timestamp.toI32();
+  event.save();
+}
